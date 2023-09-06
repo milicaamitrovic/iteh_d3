738a9399ca9b66c987c7bd1cb33139351f42d36c
Milica Mitrovic <milicamitrovicfon@gmail.com>
2023-09-06 20:52:58 +0200

    Kreirana i stilizovana Appointment komponenta

---
+++ src/components/Appointment.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Button } from "./Button";
+
+function Appointment({ obj, deleteAnAppointment }) {
+  return (
+    <>
+      <li className="cards__item">
+        <div className="cards__item__link">
+          <figure
+            className="cards__item__pic-wrap1"
+            data-category={"Zakazan termin"}>
+            <img className="cards__item__img" src={obj.src} alt="Slika" />
+          </figure>
+          <div className="cards__item__info">
+            <h5 className="cards__item__title1">{obj.name}</h5>
+            <p>{obj.date}</p>
+            <div className="star-container">
+              <ul>
+                <li>
+                  <Button
+                    className="btns"
+                    buttonStyle="btn--primary"
+                    buttonSize="btn--medium"
+                    link={"/appointments"}
+                    id={obj.id}
+                    onClick={deleteAnAppointment}>
+                    OTKAŽI TERMIN
+                  </Button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </li>
+    </>
+  );
+}
+
+export default Appointment;
--- src/components/pages/Appointments.js
+++ src/components/pages/Appointments.js
@@ -1,7 +1,54 @@
 import React from "react";
+import Appointment from "../Appointment";
+import "../Appointments.css";
+import { Button } from "../Button";
 
-function Appointments() {
-  return <div></div>;
+function Appointments({ data, deleteAnAppointment }) {
+  const headingStyle = {
+    color: "#fff",
+    "margin-top": "-50px",
+  };
+
+  return (
+    <div>
+      {data.length === 0 ? (
+        <>
+          <div className="cards1">
+            <h1 style={headingStyle}>Trenutno ni jedan termin nije zakazan!</h1>
+            <p className="app-text">Zakaži jedan već sad!</p>
+            <div className="main-btns">
+              <Button
+                buttonStyle="btn--outline"
+                buttonSize="btn--large"
+                onClick={() => {
+                  window.location.href = "/danceservices";
+                }}>
+                PRONAĐI SVOJ IDEALNI PLESNI STIL!
+              </Button>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="cards2">
+            <h1>TVOJI ZAKAZANI TERMINI</h1>
+            <div className="cards__container">
+              <div className="cards__wrapper">
+                <ul className="cards__items1">
+                  {data.map((item) => (
+                    <Appointment
+                      obj={item}
+                      deleteAnAppointment={deleteAnAppointment}
+                    />
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Appointments;
